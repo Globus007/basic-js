22fa8e91328bfb88a1b5553e018f70d9776fb303
Gleb Stashinsky <tragy@list.ru>
2021-03-14 21:48:04 +0300

    feat: solve transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  if ( !Array.isArray(array) ) throw new Error('Not array');
+  let finalArr = [];
+  let wasDeleted = false;
+  for (let i=0; i<array.length; i++) {
+      switch (array[i]) {
+        case "--discard-prev": 
+          if( wasDeleted ) {
+            wasDeleted = false;
+            break;
+          }
+          finalArr.pop();
+          break;
+        case "--double-prev" : 
+          if( i === 0 || wasDeleted ) {
+            wasDeleted = false;
+            break;
+          }
+          finalArr.push(array[i-1]);
+          break;
+        case "--double-next" :
+          if( i === array.length-1 ) {
+            wasDeleted = false;
+            break;
+          }
+          finalArr.push(array[i+1]);
+          wasDeleted = false;
+          break;
+        case "--discard-next":
+          i++;
+          wasDeleted = true;
+          break;
+        default: 
+        wasDeleted = false;
+          finalArr.push(array[i]);
+    }
+  }
+  return finalArr;
 };
